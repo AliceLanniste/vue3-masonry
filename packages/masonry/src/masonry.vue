074b46aae9d850ddb3e87db2de4e0d693005c321
Alice Peng <1399789151@qq.com>
2024-07-13 09:20:16 +0800

    init commit

---
+++ packages/masonry/src/masonry.vue
@@ -0,0 +1,191 @@
+<script setup lang="ts">
+import { reactive,ref,computed,onMounted } from 'vue'
+ interface IVirtualWaterFallProps {
+  gap: number; // 卡片间隔
+  column: number; // 瀑布流列数
+  bottom:number;
+  pageSize: number; // 单次请求数据数量
+  request?: (page: number, pageSize: number) => Promise<ICardItem[]>; // 数据请求方法
+}
+
+interface ICardItem {
+  id: number | string;
+  url:string;
+  width: number;
+  height: number;
+  [key: string]: any;
+}
+
+
+
+interface ICardPos {
+  width: number;
+  height: number;
+  x:number;
+  y:number;
+}
+
+const props = defineProps<IVirtualWaterFallProps>();
+
+defineSlots<{
+  item(props: { item: ICardItem; index: number }): any;
+}>();
+const dataState = reactive({
+     isFinish:false,
+     page:1,
+     cardWidth:0,
+     cardList:[] as IcardItem[],
+     cardPos:[] as ICardPos[],
+     loading:false,
+     columnHeight:  new Array(props.column).fill(0) as number[]
+});
+
+
+const containerRef = ref<HTMLDivElement | null>(null);
+const scrollState = reactive({
+  viewWidth:0,
+  viewHeight: 0,
+  start:0
+})
+
+
+const getCardList = async (page: number, pageSize: number) => {
+  if (dataState.isFinish) return;
+  dataState.loading= true
+  const list = await props.request(page, pageSize);
+  dataState.page++;
+  if (!list.length) {
+    dataState.isFinish = true;
+    return;
+  }
+ 
+  dataState.cardList = [...dataState.cardList, ...list];
+  computedCardPos(list); // key：根据请求的数据计算卡片位置
+  dataState.loading = false
+};
+
+
+const computedWidth = async () =>{
+  const containerWidth = containerRef.value.clientWidth;
+     dataState.cardWidth = (containerWidth - props.gap * (props.column - 1)) / props.column;
+    await getCardList(dataState.page, props.pageSize);
+
+
+}
+
+const init =async ()=>{
+ if (containerRef.value) {
+      await computedWidth()
+  }
+}
+
+const minColumn = computed(() => {
+  let minIndex = -1,
+    minHeight = Infinity;
+
+  dataState.columnHeight.forEach((item, index) => {
+    if (item < minHeight) {
+      minHeight = item;
+      minIndex = index;
+    }
+  });
+
+  return {
+    minIndex,
+    minHeight,
+  };
+});
+
+const computedCardPos = (list: ICardItem[]) =>{
+  list.forEach((item,index) => {
+      const cardHeight = Math.floor((item.height * dataState.cardWidth) / item.width);
+      if(index < props.column && dataState.cardList.length <= props.pageSize) {
+         dataState.cardPos.push({
+          width:dataState.cardWidth,
+          height:cardHeight,
+           x: index ? index * (dataState.cardWidth + props.gap) : 0,
+           y: 0,
+         })
+
+         dataState.columnHeight[index] = cardHeight + props.gap;
+        } else {
+          const { minIndex, minHeight} = minColumn.value
+          dataState.cardPos.push({
+            width:dataState.cardWidth,
+            height: cardHeight,
+            x: minIndex ? minIndex *(dataState.cardWidth +props.gap) : 0,
+            y:minHeight
+          })
+          dataState.columnHeight[minIndex] += cardHeight + props.gap;
+        }
+  });
+
+}
+
+const handleScroll = rafThrottle(() => {
+  const { scrollTop, clientHeight } = containerRef.value!;
+  const { minHeight } = minColumn.value
+  if (scrollTop + clientHeight >= minHeight) {
+    !dataState.loading && getCardList(dataState.page, props.pageSize);
+  }
+})
+
+ function rafThrottle(fn: Function) {
+  let lock = false;
+  return function (this: any, ...args: any[]) {
+    if (lock) return;
+    lock = true;
+    window.requestAnimationFrame(() => {
+      fn.apply(this, args);
+      lock = false;
+    });
+  };
+}
+onMounted( () => {
+  init()
+})
+
+
+
+</script>
+
+<template>
+  <div class="masonry-container" ref="containerRef" @scroll="handleScroll">
+  <div class="masonry-list">
+    <div class="masonry-item" v-for="(item,index) in dataState.cardList"
+     :key="item.id"
+     :style="{
+      width: `${dataState.cardPos[index].width}px`,
+      height: `${dataState.cardPos[index].height}px`,
+       transform: `translate(${dataState.cardPos[index].x}px, ${dataState.cardPos[index].y}px)`,
+     }">
+        <slot name="item" :item="item" :index="index"></slot>
+    </div>
+  </div>
+</div>
+
+
+</template>
+
+<style scoped lang="less">
+.masonry {
+  &-container {
+    width: 100%;
+    height: 100%;
+    overflow-y: scroll;
+    overflow-x: hidden;
+  }
+  &-list {
+    position: relative;
+    width: 100%;
+  }
+  &-item {
+    position: absolute;
+    top: 0;
+    left: 0;
+    box-sizing: border-box;
+  }
+}
+
+
+</style>
